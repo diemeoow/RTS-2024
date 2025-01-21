2a3ef1ae6cea143da2e72aab9392c06d43a839bf
diemeoow <144829791+diemeoow@users.noreply.github.com>
2025-01-21 22:07:08 +0300

    step10

--- Assets/Scripts/UnitDataSO.cs
+++ Assets/Scripts/UnitDataSO.cs
@@ -3,11 +3,27 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "UnitData", menuName = "Data/Unit")]
 public class UnitDataSO : ScriptableObject
 {
-    public string unitName;
-    public float speed;
-    public int health;
-    public int attackPower;
-    public int trainingCost;
-    public float detectionRadius;
+	public string unitName;         // Имя юнита
+	public float speed;             // Скорость
+	public int health;              // Здоровье
+	public int attackPower;         // Сила атаки
+	public int trainingCost;        // Стоимость тренировки
+	public float detectionRadius;   // Радиус обнаружения
+	public float minAttackRange;    // Минимальная дистанция атаки
+	public float maxAttackRange;    // Максимальная дистанция атаки
+	public float attackDelay;
 
+	[System.Serializable]
+	public class UnitData//я чета ваще не вкуриваю, почему для упрощения импорта/экспорта только эти атрибуты
+	{
+		public string unitName;         // Имя юнита
+		public float speed;             // Скорость
+		public int health;              // Здоровье
+		public int attackPower;         // Сила атаки
+		public int trainingCost;        // Стоимость тренировки
+		public float detectionRadius;   // Радиус обнаружения
+		public float minAttackRange;    // Минимальная дистанция атаки
+		public float maxAttackRange;    // Максимальная дистанция атаки
+		public float attackDelay;
+	}
 }
--- Assets/Scripts/UnitListSO.cs
+++ Assets/Scripts/UnitListSO.cs
@@ -1,7 +1,40 @@
 using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using static UnitDataSO;
 
 [CreateAssetMenu(fileName = "UnitList", menuName = "Data/Unit List")]
 public class UnitListSO : ScriptableObject
 {
     public UnitDataSO[] units;
+
+
+	public void SaveFromScriptableObject(UnitListSO unitListSO)
+	{
+		UnitList unitList = new UnitList
+		{
+			units = new UnitData[unitListSO.units.Length]
+		};
+
+		for (int i = 0; i < unitListSO.units.Length; i++)
+		{
+			unitList.units[i] = new UnitData
+			{
+				unitName = unitListSO.units[i].unitName,
+				speed = unitListSO.units[i].speed,
+				health = unitListSO.units[i].health,
+				attackPower = unitListSO.units[i].attackPower,
+				trainingCost = unitListSO.units[i].trainingCost,
+				detectionRadius = unitListSO.units[i].detectionRadius
+			};
+		}
+
+		SaveToJson(unitList);
+	}
+
+	[System.Serializable]
+	public class UnitList
+	{
+		public UnitData[] units; // массив данных юнитов для сериализации
+	}
 }
