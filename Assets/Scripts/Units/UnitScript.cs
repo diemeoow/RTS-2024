1c0a2aca0545bd9ae10f95d4a62d3245f659bf37
diemeoow <exxxxxxe90@gmail.com>
2025-01-30 13:35:49 +0300

    step17
    we are found demoRTS((

--- Assets/Scripts/Units/UnitScript.cs
+++ Assets/Scripts/Units/UnitScript.cs
@@ -1,10 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro; // Добавьте пространство имен TMPro
 using UnityEngine;
 
 public class UnitScript : MonoBehaviour
 {
     public string Name;
     public Sprite unitIcon;
-   
+    public TextMeshProUGUI nameUserText; // Ссылка на объект TextMeshProUGUI
+
+    void Start()
+    {
+        GameObject nameUserObject = GameObject.Find("NameUser"); // Найти объект по имени
+        if (nameUserObject != null)
+        {
+            nameUserText = nameUserObject.GetComponent<TextMeshProUGUI>();
+            if (nameUserText != null)
+            {
+                nameUserText.text = Name; // Установить текст равным значению переменной Name
+            }
+            else
+            {
+                Debug.LogError("TextMeshProUGUI component not found on NameUser object.");
+            }
+        }
+        else
+        {
+            Debug.LogError("NameUser object not found in the scene.");
+        }
+    }
 }
