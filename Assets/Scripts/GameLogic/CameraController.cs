10649e77278ff0b15e63ff3cd65091a774be311a
diemeoow <exxxxxxe90@gmail.com>
2025-02-05 09:57:29 +0300

    Update CameraController.cs

--- Assets/Scripts/GameLogic/CameraController.cs
+++ Assets/Scripts/GameLogic/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
     [Header("Movement Settings")]
     public float moveSpeed = 50f; 
     public float scrollSpeed = 100f;
+    public float edgeThreshold = 10f;
 
     [Header("Zoom Settings")]
     public float zoomSpeed = 10f;
@@ -14,27 +15,56 @@ public class CameraController : MonoBehaviour
 
     private Camera cam;
 
-    private void Start()
+    void Start()
     {
         cam = Camera.main; 
     }
 
-    private void Update()
+    void Update()
     {
         HandleMovement();
         HandleZoom();
+        HandleEdgeScroll();
     }
-
-    private void HandleMovement()
+    void HandleMovement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        Vector3 moveDirection = Vector3.zero;
 
-        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+        if (Input.GetKey(KeyCode.W))
+            moveDirection += Vector3.forward;
+        if (Input.GetKey(KeyCode.S))
+            moveDirection += Vector3.back;
+        if (Input.GetKey(KeyCode.A))
+            moveDirection += Vector3.left;
+        if (Input.GetKey(KeyCode.D))
+            moveDirection += Vector3.right;
+
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
     }
 
-  
+    void HandleEdgeScroll()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+
+        if (mousePosition.x < edgeThreshold)
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.World);
+        }
+        if (mousePosition.x > Screen.width - edgeThreshold)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+        }
+        if (mousePosition.y < edgeThreshold)
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
+        }
+        if (mousePosition.y > Screen.height - edgeThreshold)
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+
+
     private void HandleZoom()
     {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
